39587498528b8df8d5df4dffe84dfd3a8bb62bcb
israGutRa1342 <125627539+israGUT1ERR3Z@users.noreply.github.com>
2024-09-22 22:19:53 -0600

    Avance_22Sep
    Historial clínico hecho

--- app/src/main/java/com/example/footlab/HistorialFragment.kt
+++ app/src/main/java/com/example/footlab/HistorialFragment.kt
@@ -1,18 +1,42 @@
 package com.example.footlab
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.Button
+import androidx.fragment.app.*
 
 class HistorialFragment : Fragment() {
+
+    private lateinit var botonHistClinica: Button
+    private lateinit var botonResultados: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_perfil, container, false)
+        val view = inflater.inflate(R.layout.fragment_historial, container, false)
+
+        botonHistClinica = view.findViewById(R.id.botonVerHistorialClinico)
+        botonResultados = view.findViewById(R.id.botonVerResultados)
+
+
+        botonHistClinica.setOnClickListener {
+            openFragment(HistorialClinicoFragment())
+        }
+
+
+        botonResultados.setOnClickListener {
+
+        }
+
+        return view
+    }
+
+    private fun openFragment(fragment: Fragment, tag: String? = null) {
+        val fragmentTransaction: FragmentTransaction = parentFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment, tag)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
     }
 }
+
